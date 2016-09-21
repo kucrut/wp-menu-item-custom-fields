2ceb6f339723a8f245e54d9a8fcd07c6ecec373e
Kodie Grantham <kodie.grantham@gmail.com>
2016-09-21 11:22:48 -0500

    More code formatting

--- doc/menu-item-custom-fields-images.php
+++ doc/menu-item-custom-fields-images.php
@@ -170,11 +170,11 @@ class Menu_Item_Custom_Fields_Images {
 				<p class="description description-wide <?php echo esc_attr( $class ) ?>">
 					<?php printf(
 						'<label for="%1$s">%2$s<br />
-            <input type="hidden" id="%1$s" class="menu_image widefat %1$s" name="%3$s" value="%4$s" />
-            <img src="" id="menu_image_preview_%1$s" data-for-name="%3$s" data-for-id="%1$s" class="menu_image_preview" style="display:block;width:100px;height:auto;" />
-            <input type="button" id="menu_image_upload_%1$s" data-for-name="%3$s" data-for-id="%1$s" class="menu_image_upload" value="Upload Image" />
-            <input type="button" id="menu_image_clear_%1$s" data-for-name="%3$s" data-for-id="%1$s" class="menu_image_clear" value="Clear Image" />
-            </label>',
+						<input type="hidden" id="%1$s" class="menu_image widefat %1$s" name="%3$s" value="%4$s" />
+						<img src="" id="menu_image_preview_%1$s" data-for-name="%3$s" data-for-id="%1$s" class="menu_image_preview" style="display:block;width:100px;height:auto;" />
+						<input type="button" id="menu_image_upload_%1$s" data-for-name="%3$s" data-for-id="%1$s" class="menu_image_upload" value="Upload Image" />
+						<input type="button" id="menu_image_clear_%1$s" data-for-name="%3$s" data-for-id="%1$s" class="menu_image_clear" value="Clear Image" />
+						</label>',
 						esc_attr( $id ),
 						esc_html( $label ),
 						esc_attr( $name ),
